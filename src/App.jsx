2c510570570b012646921547d7e34ce380a172d6
xIsra <israelkouperman@gmail.com>
2019-10-21 23:47:26 +0300

    UI spinner

--- src/App.jsx
+++ src/App.jsx
@@ -4,57 +4,98 @@ import './app.scss';
 
 
 
-function App() {
-  return (
-    <div className="App">
-      <div className="main-section">
-        <h1>Select your place!</h1>
+class App extends React.Component {
+  state = {
+    index: null,
+    active: false,
+    sliceCount: 10,
 
-        <div class="roulette">
-          <p class="love">Clic en Spin para iniciar!</p>
-          <div class="spinner">
-            <div class="triangle">
-              <span class="text">1</span>
+  }
+  constructor(props) {
+    super(props);
+    this.rouletteRef = React.createRef();
+  }
+  spinRandom = () => {
+    const { sliceCount, active } = this.state;
+    // const randomIndex = parseInt(Math.random() * sliceCount);
+    // const classString = this.rouletteRef.current.className;
+    this.setState({
+      index: parseInt(Math.random() * sliceCount),
+      active: active ? false : true,
+    })
+    // setTimeout(()=>{
+    //   this.setState({
+    //     // index: null,
+    //     active: false,
+    //   })
+    // },600)
+  }
+
+  render() {
+    const { index, active } = this.state;
+    return (
+      <div className="App">
+        <div className="main-section">
+          <h1>Select your place!</h1>
+
+          <div className="roulette" ref={this.rouletteRef}>
+            <p className="love">CLick the SPINNER!</p>
+            <div className={"spinner " + (index != null ? " index-" + index : "") + (active?" spin":"")}>
+              <div className="triangle">
+                <img src="https://picsum.photos/650/450" alt="" srcset=""/>
+                {/* <span className="text">1</span> */}
+              </div>
+              <div className="triangle">
+                <img src="https://picsum.photos/650/450" alt="" srcset=""/>
+                {/* <span className="text">2</span> */}
+              </div>
+              <div className="triangle">
+                <img src="https://picsum.photos/650/450" alt="" srcset=""/>
+                {/* <span className="text">3</span> */}
+              </div>
+              <div className="triangle">
+                <img src="https://picsum.photos/650/450" alt="" srcset=""/>
+                {/* <span className="text">4</span> */}
+              </div>
+              <div className="triangle">
+                <img src="https://picsum.photos/650/450" alt="" srcset=""/>
+                {/* <span className="text">5</span> */}
+              </div>
+              <div className="triangle">
+                <img src="https://picsum.photos/650/450" alt="" srcset=""/>
+                {/* <span className="text">6</span> */}
+              </div>
+              <div className="triangle">
+                <img src="https://picsum.photos/650/450" alt="" srcset=""/>
+                {/* <span className="text">7</span> */}
+              </div>
+              <div className="triangle">
+                <img src="https://picsum.photos/650/450" alt="" srcset=""/>
+                {/* <span className="text">8</span> */}
+              </div>
+              <div className="triangle">
+                <img src="https://picsum.photos/650/450" alt="" srcset=""/>
+                {/* <span className="text">9</span> */}
+              </div>
+              <div className="triangle">
+                <img src="https://picsum.photos/650/450" alt="" srcset=""/>
+                {/* <span className="text">10</span> */}
+              </div>
+              <div className="triangle">
+                <img src="https://picsum.photos/650/450" alt="" srcset=""/>
+                {/* <span className="text">11</span> */}
+              </div>
+              <div className="triangle">
+                <img src="https://picsum.photos/650/450" alt="" srcset=""/>
+                {/* <span className="text">12</span> */}
+              </div>
             </div>
-            <div class="triangle">
-              <span class="text">2</span>
-            </div>
-            <div class="triangle">
-              <span class="text">3</span>
-            </div>
-            <div class="triangle">
-              <span class="text">4</span>
-            </div>
-            <div class="triangle">
-              <span class="text">5</span>
-            </div>
-            <div class="triangle">
-              <span class="text">6</span>
-            </div>
-            <div class="triangle">
-              <span class="text">7</span>
-            </div>
-            <div class="triangle">
-              <span class="text">8</span>
-            </div>
-            <div class="triangle">
-              <span class="text">9</span>
-            </div>
-            <div class="triangle">
-              <span class="text">10</span>
-            </div>
-            <div class="triangle">
-              <span class="text">11</span>
-            </div>
-            <div class="triangle">
-              <span class="text">12</span>
-            </div>
+            <div id="like" className="button button-spin" onClick={this.spinRandom}><span>SPIN</span></div>
           </div>
-          <div id="like" class="button button-spin" onclick="spin()"><span>SPIN</span></div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
